b98dd189991ca0b0d5293cc0ef9bcea0eb7ce7ca
Кирилл Кувшинов <k.koovshinov@gmail.com>
2024-01-15 19:19:20 +0300

    Roll back a bit due to a persistent 400 err.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,12 +8,20 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-SECRET_KEY = os.getenv("SECRET_KEY", "defaultVal")
+# SECRET_KEY = os.getenv("SECRET_KEY", "defaultVal")
 
-DEBUG = os.getenv("DEBUG", "false").lower() == "true"
+# DEBUG = os.getenv("DEBUG", "false").lower() == "true"
+
+# ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost").split()
+
+
+SECRET_KEY = (
+    "django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^"
+)
+
+DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
