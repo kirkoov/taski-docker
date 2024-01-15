fcf2ff263c590a25ca73e50584ced9a8691c6d79
Кирилл Кувшинов <k.koovshinov@gmail.com>
2024-01-15 19:35:54 +0300

    Find the 400 err culprit.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,23 +8,15 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# SECRET_KEY = os.getenv("SECRET_KEY", "defaultVal")
+SECRET_KEY = os.getenv("SECRET_KEY", "defaultVal")
 
 # DEBUG = os.getenv("DEBUG", "false").lower() == "true"
-
-# ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost").split()
-
-
-SECRET_KEY = (
-    "django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^"
-)
 
 DEBUG = True
 
 ALLOWED_HOSTS = []
 
 # Application definition
-
 INSTALLED_APPS = [
     "api.apps.ApiConfig",
     "django.contrib.admin",
